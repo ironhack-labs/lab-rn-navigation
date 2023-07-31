29bf414eff030e1fabab52e1a579b4a2176ea7ef
Geyser Ramírez <geyser.grs@gmail.com>
2023-07-31 07:19:37 -0600

    avances de la navegación

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Button, View} from 'react-native';
+
+type Profile = {
+  name: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  avatar: number;
+};
+
+const myProfile: Profile = {
+  name: 'Geyser',
+  lastName: 'Ramírez',
+  email: 'geyser.gallardo@digitalfemsa.com',
+  phone: '9991369509',
+  avatar: require('../../assets/profile.png'),
+};
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <View>
+      <Button
+        title="Profile"
+        onPress={() => navigation.navigate('Profile', myProfile)}
+      />
+      <Button title="Team members" />
+    </View>
+  );
+};
+
+export default HomeScreen;
---
+++ src/screens/TeamScreen.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TeamScreen = () => {
+  return <></>;
+};
+
+export default TeamScreen;
